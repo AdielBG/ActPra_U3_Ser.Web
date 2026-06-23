9b1137780b9a644c922185b04137a0e8460649bd
AdielBG <adielbg17.2@gmail.com>
2026-06-23 16:44:21 -0400

    DbContext

---
+++ ActPra_U3_Ser.Web/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiBiblioteca_P3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBiblioteca_P3.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
