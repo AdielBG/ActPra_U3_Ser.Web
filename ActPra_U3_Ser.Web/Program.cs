eb5651108fcd3e7fa9096afb243824b833b6bb6f
AdielBG <adielbg17.2@gmail.com>
2026-06-23 16:55:21 -0400

    Configuración del Program

--- ActPra_U3_Ser.Web/Program.cs
+++ ActPra_U3_Ser.Web/Program.cs
@@ -1,11 +1,33 @@
+using ApiBiblioteca_P3.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Registra controladores
 builder.Services.AddControllers();
+
+// Registra Entity Framework con SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configura Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ApiBiblioteca P3",
+        Version = "v1",
+        Description = "API REST para gestión de catálogo de libros con acceso a base de datos."
+    });
+});
+
 
 var app = builder.Build();
 
