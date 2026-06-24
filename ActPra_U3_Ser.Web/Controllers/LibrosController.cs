c7a2aeb484d9075e6ee7472a36d5c9fb1ebbc87a
AdielBG <adielbg17.2@gmail.com>
2026-06-24 12:45:10 -0400

    LibrosController

---
+++ ActPra_U3_Ser.Web/Controllers/LibrosController.cs
@@ -0,0 +1,18 @@
+﻿using ApiBiblioteca_P3.Data;
+using ApiBiblioteca_P3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBiblioteca_P3.Controllers
+{
+    [ApiController]
+    [Route("api/libros")]
+    public class LibrosController : ControllerBase
+    {
+        
+
+
+
+
+    }
+}
